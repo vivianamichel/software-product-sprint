d60ecd2df3a67431aaf09fbd28a9288204f60565
Viviana Michel <michelvivianaa@gmail.com>
2020-08-10 00:50:28 +0000

    finished scheduler

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,53 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> possibleMeetingTimes = new ArrayList<TimeRange>();
+    ArrayList<TimeRange> busyTimes = new ArrayList<TimeRange>();
+
+    for(Event event: events){
+        TimeRange eventTimeRange = event.getWhen();
+        for(String attendee: event.getAttendees()){
+            if (request.getAttendees().contains(attendee)){
+                if (busyTimes.isEmpty()){
+                    busyTimes.add((TimeRange.fromStartDuration(eventTimeRange.start(), eventTimeRange.duration())));
+                } else if(busyTimes.get(busyTimes.size()-1).overlaps(eventTimeRange)){
+                    if(busyTimes.get(busyTimes.size()-1).start() < eventTimeRange.start() && busyTimes.get(busyTimes.size()-1).end() > eventTimeRange.end()){
+                        busyTimes.set(busyTimes.size()-1, (TimeRange.fromStartEnd(busyTimes.get(busyTimes.size()-1).start(), busyTimes.get(busyTimes.size()-1).end() - 1, true)));
+                    } else if(busyTimes.get(busyTimes.size()-1).end() > eventTimeRange.start()){
+                        busyTimes.set(busyTimes.size()-1, (TimeRange.fromStartEnd(busyTimes.get(busyTimes.size()-1).start(), eventTimeRange.end() - 1, true)));
+                    }
+                } else {
+                    busyTimes.add((TimeRange.fromStartDuration(eventTimeRange.start(), eventTimeRange.duration())));
+                }
+            }
+        }
+    }
+    if (busyTimes.isEmpty()){
+        if (request.getDuration() > TimeRange.WHOLE_DAY.duration()){
+            return possibleMeetingTimes;
+        } 
+        possibleMeetingTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true));
+        return possibleMeetingTimes;
+    }
+
+    for(int i = 0; i < busyTimes.size(); i++){
+        if (i==0) {
+            if (busyTimes.get(i).start() - TimeRange.START_OF_DAY >= request.getDuration()){
+                possibleMeetingTimes.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, busyTimes.get(i).start() - 1, true));
+            }
+        } 
+        if(i + 1 == busyTimes.size()){
+           if (TimeRange.END_OF_DAY - busyTimes.get(i).end() >= request.getDuration()){
+               possibleMeetingTimes.add(TimeRange.fromStartEnd(busyTimes.get(i).end(), TimeRange.END_OF_DAY, true));
+           }
+        } else if(busyTimes.get(i+1).start() - busyTimes.get(i).end() >= request.getDuration()){
+                possibleMeetingTimes.add(TimeRange.fromStartEnd(busyTimes.get(i).end(), busyTimes.get(i+1).start() - 1,  true));
+        }
+    }
+    return possibleMeetingTimes;
   }
 }
